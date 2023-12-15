a5e8a23daceedaf24c0b5c1fb68ba56675b51671
vaidya1111 <sagar.vaidya11@gmail.com>
2023-12-15 14:15:24 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,28 @@
+const heading= React.createElement(
+    "h1",
+    {id:"heading"},
+    "Hello world from react!"
+    );
+
+//heading is an object
+console.log(heading)
+
+/*
+<div id="parent">
+    <div id="child">
+        <h1> I am a h1 tag </h1>
+    </div>
+</div>
+*/
+
+ const parent = React.createElement(
+ "div",
+ {id:"parent"},
+ React.createElement(
+    "div",
+    {id:"child"},
+    [React.createElement("h1",{},"i am a h1 tag"),React.createElement("h2",{},"i am a h2 tag")])
+ );
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
